5a5b4427e7f5e21edb9a29d7cd45fcb833067026
Narita Ryosuke <ryo13chan@gmail.com>
2026-03-29 16:27:13 +0900

    shadcn追加

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js'
+
+	const test = () => alert()
+</script>
+
+<Button onclick={test}>Click me</Button>
 <h1 class="text-9xl">Welcome to SvelteKit</h1>
 <p class="mt-4 text-lg text-gray-600">ポートフォリオサイトへようこそ</p>
