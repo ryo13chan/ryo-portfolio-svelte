c4d936a36d70e830393fe6cb99eb3618f1964f59
Narita Ryosuke <ryo13chan@gmail.com>
2026-03-29 16:39:17 +0900

    アイコン追加

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button/index.js'
+	import Camera from '@lucide/svelte/icons/camera'
 
 	const test = () => alert()
 </script>
 
 <Button onclick={test}>Click me</Button>
+<Button size="icon">
+	<Camera />
+</Button>
 <h1 class="text-9xl">Welcome to SvelteKit</h1>
 <p class="mt-4 text-lg text-gray-600">ポートフォリオサイトへようこそ</p>
